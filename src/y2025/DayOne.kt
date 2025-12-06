49138e5a83bf219344a41885f8dbfb01c4e622d3
Cal <cwolfson58@gmail.com>
2025-12-06 13:00:18 +1100

    Day One

---
+++ src/y2025/DayOne.kt
@@ -0,0 +1,34 @@
+package y2025
+
+import java.io.File
+
+object DayOne {
+
+    fun solution() {
+        val rotationsFile = File("resources/2025/rotations.txt")
+        val lines = rotationsFile.readLines()
+        val start = System.nanoTime()
+        var currentNumber = 50
+        var currentCount = 0
+
+        lines.forEach { line ->
+            val firstChar = line.first()
+            var number = line.substring(1).toInt()
+            val extraTimes = number / 100
+            currentCount += extraTimes
+            number -= extraTimes * 100
+            if (firstChar == 'L') {
+                if (currentNumber != 0 && number >= currentNumber) currentCount++
+                currentNumber -= number
+            } else {
+                if (currentNumber + number >= 100) currentCount++
+                currentNumber += number
+            }
+            currentNumber = (currentNumber + 100) % 100
+        }
+        val totalNs = System.nanoTime() - start
+        println("totalNS: $totalNs")
+        println("current: $currentCount")
+    }
+
+}
