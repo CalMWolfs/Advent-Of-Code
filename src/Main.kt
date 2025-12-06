1e3a1541537222278822368ecbd5a2fc270403d2
Cal <cwolfson58@gmail.com>
2025-12-06 13:07:14 +1100

    Working File

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+import java.io.File
+
+fun main() {
+//    val file = File("resources/2025/--.txt")
+//    val text = file.readLines()
+//    val startTime = System.nanoTime()
+//    var total = 0
+//
+//    val totalNs = System.nanoTime() - startTime
+//    println("totalNS: $totalNs")
+//    println("total: $total")
+}
