87488b7a28e649f8ebb3468361f380875e6a72ef
Cal <cwolfson58@gmail.com>
2026-02-20 11:55:12 +1100

    Setup c++ files

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+
+#include "y2018/DayOne2018.h"
+
+int main() {
+    DayOne2018::solution();
+
+    // std::ifstream file("resources/2018/frequencies.txt");
+    //
+    // if (!file.is_open()) {
+    //     std::cerr << "Error opening file";
+    //     return;
+    // }
+    //
+    // std::string text{std::istreambuf_iterator(file), std::istreambuf_iterator<char>()};
+    //
+    // file.clear();
+    // file.seekg(0);
+    //
+    // std::vector<std::string> lines;
+    // std::string readingLine;
+    // while (std::getline(file, readingLine)) {
+    //     lines.push_back(readingLine);
+    // }
+    //
+    // const auto startTime = std::chrono::high_resolution_clock::now();
+    //
+    // int total = 0;
+    //
+    // const auto endTime = std::chrono::high_resolution_clock::now();
+    // const auto totalNs = std::chrono::duration_cast<std::chrono::nanoseconds>(
+    //     endTime - startTime
+    // ).count();
+    //
+    // std::cout << "totalNS: " << totalNs << "\n";
+    // std::cout << "total: " << total << "\n";
+
+    return 0;
+}
