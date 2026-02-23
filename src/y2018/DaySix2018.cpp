102e45d44de661fc1bfc239abc43426e6fb566c0
Cal <cwolfson58@gmail.com>
2026-02-23 16:15:20 +1100

    Day Six (2018)

--- src/y2018/DaySix2018.cpp
+++ src/y2018/DaySix2018.cpp
@@ -3,19 +3,28 @@
 #include <chrono>
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <set>
 #include <vector>
 
 class Coordinate {
 public:
+    int id;
     size_t x;
     size_t y;
 
-    explicit Coordinate(const std::string &str) {
+    Coordinate(const std::string &str, const int number) {
+        id = number;
         const size_t splitPos = str.find(',');
         x = static_cast<size_t>(std::stoi(str.substr(0, splitPos)));
         y = static_cast<size_t>(std::stoi(str.substr(splitPos + 1)));
     }
 };
+
+int manhattanDistance(size_t x1, size_t y1, size_t x2, size_t y2) {
+    return std::abs(static_cast<int>(x1) - static_cast<int>(x2)) +
+           std::abs(static_cast<int>(y1) - static_cast<int>(y2));
+}
 
 void DaySix2018::solution() {
     std::ifstream file("resources/2018/coordinates.txt");
@@ -37,19 +46,32 @@ void DaySix2018::solution() {
 
     std::vector<Coordinate> coordinates;
 
+    int id = 0;
     for (const std::string &line: lines) {
-        coordinates.emplace_back(line);
+        coordinates.emplace_back(line, ++id);
     }
 
     size_t maxX = 0;
     size_t maxY = 0;
 
     for (auto &coordinate: coordinates) {
-        maxX = std::max(maxX, coordinate.x);
-        maxY = std::max(maxY, coordinate.y);
+        maxX = std::max(maxX, coordinate.x + 1);
+        maxY = std::max(maxY, coordinate.y + 1);
     }
 
-    std::vector grid(maxX, std::vector(maxY, 0));
+    for (size_t y = 0; y < maxY; ++y) {
+        for (size_t x = 0; x < maxX; ++x) {
+            int totalDistance = 0;
+
+            for (const auto &coordinate: coordinates) {
+                const int dist = manhattanDistance(coordinate.x, coordinate.y, x, y);
+                totalDistance += dist;
+            }
+            if (totalDistance < 10000) {
+                total++;
+            }
+        }
+    }
 
     const auto endTime = std::chrono::high_resolution_clock::now();
     const auto totalNs = std::chrono::duration_cast<std::chrono::nanoseconds>(
